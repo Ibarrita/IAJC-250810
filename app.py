23883a3baba652350f61c0f18f0d98ec6cffb7c8
José Ibarra <204079627+Ibarrita@users.noreply.github.com>
2025-10-15 08:40:06 -0500

    ruta

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def about():
 def signup():
     return render_template("signup.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
